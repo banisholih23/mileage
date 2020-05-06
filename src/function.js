668946cde238116663ca033ccb693d2851605a2c
banisholih23 <banisholih23@gmail.com>
2020-05-06 15:11:18 +0700

    mileage

--- src/function.js
+++ src/function.js
@@ -1,13 +1,14 @@
 module.exports function mileage(waktuAwal, waktuAkhir) {
     var vo = 6
-    for(let t=10; t<=20; t + 2) {
+    for(let t=10.1221; t<=10.17.21; t + 2) {
         var a = 2
         if(t >= 300) {
             a = 8
         }
-        for(let t=20; t<=30r; t + 1)
+        for(let t=10.1721; t<=12.0000; t + 1)
             a = 9
             var s = vo * t + a * t * t / 2
     }
     console.log(s)
 }
+
